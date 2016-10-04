c4b5d62615287eb407a6d705df849940ec339674
GetRektByMe <byronvanstien@rocketmail.com>
2016-10-04 03:31:36 +0100

    remove safe_roles()

--- pixie/utils/__init__.py
+++ pixie/utils/__init__.py
@@ -12,14 +12,3 @@ user_agent = "Pixie (https://github.com/GetRektByMe/Pixie)"
 # A function to use with checks to check for owner
 def is_owner(ctx):
     return ctx.message.author.id == setup_file["discord"]['owner_id']
-
-
-# Sanitises roles so when functions that print roles are used it doesn't ping @everyone
-def safe_roles(roles: list):
-    names = []
-    for role in roles:
-        if role.name == "@everyone":
-            names.append("@\u200beveryone")  # \u200b is an invisible space
-        else:
-            names.append(role.name)
-    return names
